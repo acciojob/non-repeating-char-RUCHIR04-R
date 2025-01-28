af40a862622f32f1f41d9c34372d0767817e2739
RUCHIR04-R <ruchirgawture@gmail.com>
2025-01-28 08:31:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+const charConst = {};
+	for(let i = 0; i < str.length; i++)
+		{
+			const char = str[i];
+			charConst[char] = (charConst[char] || 0) + 1;
+		}
+	for(let  i =0; i < str.length; i++){
+		const char = str[i];
+		if (charConst[char] === 1){
+			return char;
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
+J
